fef1d04bf1b3d792522c6686012072ba2f40f96b
王银举 <aaronwang@Mac.bbrouter>
2025-11-29 17:39:47 +0800

    fix port & deps

--- api/server.ts
+++ api/server.ts
@@ -272,3 +272,6 @@ initializeServices();
 export { io };
 
 export default app;
+
+const port = process.env.PORT || 3001;
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
